48ad1ef5fd6317cf012dc781cf3c3852832ae332
roberta soliman donofreo <47365108+robertasolimandonofreo@users.noreply.github.com>
2022-01-18 10:37:55 -0300

    Criando configurações do terraform

---
+++ terraform/nginx-variables.tf
@@ -0,0 +1,30 @@
+variable "nginx_app_name" {
+  description = "Name of Application Container"
+  default     = "nginx"
+}
+
+variable "nginx_app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "robertasolimandonofreo/webserver:latest"
+}
+
+variable "nginx_app_port" {
+  description = "Port exposed by the Docker image to redirect traffic to"
+  default     = 80
+}
+
+variable "nginx_app_count" {
+  description = "Number of Docker containers to run"
+  default     = 2
+}
+
+variable "nginx_fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "1024"
+}
+
+variable "nginx_fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "2048"
+}
+
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,10 @@
+app_name        = "clicksign-nginx"
+app_environment = "prod" 
+aws_access_key    = ""
+aws_secret_key    = ""
+aws_key_pair_name = "terraform"
+aws_key_pair_file = "terraform.pem"
+aws_region        = "us-east-1"
+
+app_sources_cidr   = ["0.0.0.0/0"] 
+admin_sources_cidr = ["0.0.0.0/0"] 
